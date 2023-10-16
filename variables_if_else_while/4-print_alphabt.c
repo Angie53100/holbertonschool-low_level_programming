4e20f6b40e7ba81f1c858e9c06774b914b9eac5e
Angie53100 <a.bernard53@hotmail.fr>
2023-10-16 11:05:06 -0700

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -8,11 +8,12 @@
  */
 int main(void)
 { char letter;
-	for (letter = 'a'; letter <= 'z'; letter++)
-		if (letter != 'e' && letter != 'q'
-				putchar(letter);
-				putchar('\n');
 
-				return (0);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+       for (letter != 'e' && letter != 'q')
+		putchar('\n');
+
+		return (0);
 }
 
